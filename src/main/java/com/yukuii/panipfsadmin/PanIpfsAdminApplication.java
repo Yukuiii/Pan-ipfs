cce20ea8fc189cc5b76f2f253fbb00e3f8902fee
Yukuiii <472619563@qq.com>
2024-11-08 21:43:25 +0800

    init

---
+++ src/main/java/com/yukuii/panipfsadmin/PanIpfsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.yukuii.panipfsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanIpfsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanIpfsAdminApplication.class, args);
+    }
+
+}
