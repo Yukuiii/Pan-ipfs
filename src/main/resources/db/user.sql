8e52bd50f408eddf7f9f406b1dfe809b56decbcc
Yukuiii <472619563@qq.com>
2024-11-08 22:22:12 +0800

    更新sql

--- src/main/resources/db/user.sql
+++ src/main/resources/db/user.sql
@@ -3,9 +3,9 @@ CREATE TABLE `user` (
     `username` varchar(50) NOT NULL COMMENT '用户名',
     `password` varchar(100) NOT NULL COMMENT '密码',
     `nickname` varchar(50) NOT NULL COMMENT '昵称',
-    `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
-    `create_time` datetime NOT NULL COMMENT '创建时间',
-    `update_time` datetime NOT NULL COMMENT '更新时间',
+    `email` varchar(100) NOT NULL COMMENT '邮箱',
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `uk_username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表'; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表';
